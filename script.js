b28f077b6c7ded8d7f3f7262a2b937596a858c2d
andreydro <andreydro14@gmail.com>
2018-06-29 12:38:35 +0300

    Add basic routes

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+window.onhashchange = function(){
+	displayPage(window.location.hash || "#main");
+};
+
+displayPage(window.location.hash || "#main");
+
+function displayPage(hash) {
+	if (hash == "#main") {
+		displayMap();
+	} else if (hash == "#about") {
+		displayAbout();
+	} else if (hash == "#author") {
+		displayAuthor();
+	}
+}
+
+function displayMap() {
+	var content = document.getElementById("content");
+	content.innerHTML = "";
+
+	var mainPage = document.getElementById("main");
+	content.innerHTML = mainPage.innerHTML;
+}
+
+function displayAuthor() {
+  var content = document.getElementById("content");
+  var author = document.getElementById("author");
+  content.innerHTML = author.innerHTML;
+}
+
+function displayAbout() {
+	var content = document.getElementById("content");
+	content.innerHTML = "";
+
+	var about = document.getElementById("about");
+	content.innerHTML = about.innerHTML;
+}
